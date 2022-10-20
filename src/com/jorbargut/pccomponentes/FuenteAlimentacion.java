35ec8160f128478d87bcd429239f413273cea4f5
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-10-20 18:38:41 +0200

    PcComponentes

--- src/com/jorbargut/pccomponentes/FuenteAlimentacion.java
+++ src/com/jorbargut/pccomponentes/FuenteAlimentacion.java
@@ -1,4 +1,26 @@
 package com.jorbargut.pccomponentes;
 
 public class FuenteAlimentacion {
+    /**
+     * Atributos
+     */
+    private Integer id;
+    private String brand;
+    private String model;
+    private String memory;
+    private String maker;
+
+    /**
+     * Metodos
+     */
+    public Integer getId(){return id;}
+    public void  setId(Integer id){this.id = id;}
+    public String getBrand(){return brand;}
+    public void setBrand(String brand){this.brand = brand;}
+    public String getModel(){return model;}
+    public void setModel(String model){this.model = model;}
+    public String getMemory(){return memory; }
+    public void setMemory(String memory){this.memory = memory;}
+    public String getMaker(){return maker;}
+    public void setMaker(String maker) {this.maker = maker;}
 }
--- src/com/jorbargut/pccomponentes/Ventilador.java
+++ src/com/jorbargut/pccomponentes/Ventilador.java
@@ -1,4 +1,26 @@
 package com.jorbargut.pccomponentes;
 
 public class Ventilador {
+    /**
+     * Atributos
+     */
+    private Integer id;
+    private String brand;
+    private String model;
+    private String watts;
+    private String maker;
+
+    /**
+     * Metodos
+     */
+    public Integer getId(){return id;}
+    public void  setId(Integer id){this.id = id;}
+    public String getBrand(){return brand;}
+    public void setBrand(String brand){this.brand = brand;}
+    public String getModel(){return model;}
+    public void setModel(String model){this.model = model;}
+    public String getWatts(){return watts; }
+    public void setWatts(String watts){this.watts = watts;}
+    public String getMaker(){return maker;}
+    public void setMaker(String maker) {this.maker = maker;}
 }
