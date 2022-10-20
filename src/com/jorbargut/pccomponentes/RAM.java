24d5ede810e55e636ff59a1ca6bc779fd64ff347
Jorge Barrio <115726013+Jhinsso@users.noreply.github.com>
2022-10-20 18:05:46 +0200

    Creacion de una clase para la targeta grafica.

--- src/com/jorbargut/pccomponentes/RAM.java
+++ src/com/jorbargut/pccomponentes/RAM.java
@@ -12,12 +12,12 @@ public class RAM {
     /**
      * Metodos
      */
-    public String getId(){return id}
-    public void setId(String id){this.id=id}
-    public String getBrand(){return brand}
-    public void setBrand(String brand){this.brand=brand}
-    public String getModel(){return model}
-    public void setModel(String model){this.model=model}
-    public String getMemory(){return memory}
-    public void setMaker(String maker){this.maker=maker}
+    public String getId(){return id;}
+    public void setId(String id){this.id=id;}
+    public String getBrand(){return brand;}
+    public void setBrand(String brand){this.brand=brand;}
+    public String getModel(){return model;}
+    public void setModel(String model){this.model=model;}
+    public String getMemory(){return memory;}
+    public void setMaker(String maker){this.maker=maker;}
 }
